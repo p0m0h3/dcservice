30bdc609774ab826c4722f9b695b8c4cf6491953
Pouria Mokhtari <contact@pouriamokhtari.ir>
2022-01-01 18:41:20 +0330

    implement tool directory

--- api/__init__.py
+++ api/__init__.py
@@ -9,9 +9,13 @@ from .schemas import Task, TaskResult
 def create_app() -> FastAPI:
     app = FastAPI()
 
-    @app.post("/task")
-    def read_root(task: Task) -> TaskResult:
+    @app.get("/tools")
+    def get_tools():
+        return service.tools
+
+    @app.post("/task", response_model=TaskResult)
+    def read_root(task: Task):
         task_id = service.start_task(task.tool, args=task.args)
-        return {'task_id': task_id}
+        return {"task_id": task_id}
 
     return app

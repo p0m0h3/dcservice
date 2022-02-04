e0e8ebb51405b853e7cbd579490cf8d405124ad1
Pouria Mokhtari <contact@pouriamokhtari.ir>
2022-02-04 07:42:38 +0330

    add api metadata to documentation

--- api/__init__.py
+++ api/__init__.py
@@ -4,7 +4,24 @@ from .task import router as task
 
 
 def create_app() -> FastAPI:
-    app = FastAPI()
+    description = """
+dcservice or Docker Computation Service is a docker based sand-boxed task processing HTTP API implementation.
+It takes a set of defined tools (as docker images) and their respective input and listens for task requests on an HTTP REST API.
+    """
+    app = FastAPI(
+        title="DCSERVICE",
+        description=description,
+        version="0.1",
+        contact={
+            "name": "Pouria Mokhtari",
+            "url": "https://github.com/pouriamokhtari/dcservice",
+            "email": "contact@pouriamokhtari.ir",
+        },
+        license_info={
+            "name": "MIT License",
+            "url": "https://mit-license.org/",
+        },
+    )
 
     app.include_router(task)
 
